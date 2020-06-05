c8838ee90abb3e5120404b08858c667e2b76bb52
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-06-04 23:16:55 -0300

    - Groovy Template

--- src/main/java/com/viglet/shio/groovy/ShGroovy.java
+++ src/main/java/com/viglet/shio/groovy/ShGroovy.java
@@ -1,12 +1,17 @@
 package com.viglet.shio.groovy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 import groovy.util.GroovyScriptEngine;
 
 public class ShGroovy {
-
     public static void main(String[] args) {
 
         System.out.println("Hello World");
@@ -15,8 +20,17 @@ public class ShGroovy {
         binding.setVariable("language", "Groovy");
 
         GroovyShell shell = new GroovyShell(binding);
-
-        System.out.println(shell.evaluate("(1..10).sum()"));
+        InputStream is = ShGroovy.class.getClassLoader().getResourceAsStream("groovy/template.groovy");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        
+        try {
+            String s = org.apache.commons.io.IOUtils.toString(br);
+            System.out.println(shell.evaluate(s));
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
 
     }
 }
